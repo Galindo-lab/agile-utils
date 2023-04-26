522511a387882222b963c0f4449f056949b00abe
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-04-26 16:17:44 -0700

    Activity renombrado a Task

--- AgileUtils/src/main/java/SCRUM/ScrumProyect.java
+++ AgileUtils/src/main/java/SCRUM/ScrumProyect.java
@@ -1,6 +1,6 @@
 package SCRUM;
 
-import com.mycompany.agileutils.Activity;
+import com.mycompany.agileutils.Task;
 import com.mycompany.agileutils.Proyect;
 import com.mycompany.agileutils.Requirement;
 import com.mycompany.agileutils.Team;
